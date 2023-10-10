f8c94c7fe54c11e6e31efe2966839ea8b2d6a038
Fonta1n3 <fontainedenton@googlemail.com>
2023-10-09 22:55:04 -0500

    fix: prevent crash if node is off

--- Plasma/SceneDelegate.swift
+++ Plasma/SceneDelegate.swift
@@ -33,6 +33,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        signal(SIGPIPE, SIG_IGN)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
